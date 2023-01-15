772106336810f65e8ab0834f68a7a912fb55ec8d
HectorGL2021 <hector.d998@gmail.com>
2023-01-15 16:37:16 +0100

    bd

--- Notepad/database/migrations/2023_01_15_125037_create_notas_table.php
+++ Notepad/database/migrations/2023_01_15_125037_create_notas_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
     {
         Schema::create('notas', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('titulo');
+            $table->string('texto');
+            $table->timestamp('created_at');
         });
     }
 
--- Notepad/database/seeders/NotasSeeder.php
+++ Notepad/database/seeders/NotasSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Notas;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,17 @@ class NotasSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Notas::create([
+            'titulo' => 'Pendiente Akiro',
+            'texto' => 'PIERCING BAÑADO EN ORO CON ARO DE CIRCONITAS Y COLGANTE RECTANGULAR DE PIEDRA AZUL OSCURO'
+        ]);
+        Notas::create([
+            'titulo' => 'Anillo Spark',
+            'texto' => 'ANILLO DE 4 CIRCONITAS BLANCAS BRILLANTES EN PLATA CON BAÑO DE ORO'
+        ]);
+        Notas::create([
+            'titulo' => 'Collar Sentiment',
+            'texto' => 'COLLAR BAÑADO EN ORO CON COLGANTE HEXAGONAL CON CIRCONITAS BLANCAS'
+        ]);
     }
 }
