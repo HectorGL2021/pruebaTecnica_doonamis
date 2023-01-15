04cdb41162d9cbf25e2c77533fe0eef3a30b2b03
HectorGL2021 <hector.d998@gmail.com>
2023-01-15 13:55:32 +0100

    resources

---
+++ Notepad/app/Http/Controllers/NotasController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Notas;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class NotasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Notas  $notas
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Notas $notas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Notas  $notas
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Notas $notas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Notas  $notas
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Notas $notas)
+    {
+        //
+    }
+}
