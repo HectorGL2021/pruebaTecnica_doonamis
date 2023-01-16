81b440a64f06b3e32d0bd89e3a71556ef559cc7f
HectorGL2021 <hector.d998@gmail.com>
2023-01-16 19:20:54 +0100

    Seeder

--- Notepad/database/seeders/NotasSeeder.php
+++ Notepad/database/seeders/NotasSeeder.php
@@ -16,16 +16,66 @@ class NotasSeeder extends Seeder
     public function run()
     {
         Notas::create([
-            'titulo' => 'Pendiente Akiro',
-            'texto' => 'PIERCING BAÑADO EN ORO CON ARO DE CIRCONITAS Y COLGANTE RECTANGULAR DE PIEDRA AZUL OSCURO'
+            'titulo' => 'NOTA 1',
+            'texto' => 'ESTO ES UNA NOTA DE PRUEBA 1'
         ]);
         Notas::create([
-            'titulo' => 'Anillo Spark',
-            'texto' => 'ANILLO DE 4 CIRCONITAS BLANCAS BRILLANTES EN PLATA CON BAÑO DE ORO'
-        ]);
-        Notas::create([
-            'titulo' => 'Collar Sentiment',
-            'texto' => 'COLLAR BAÑADO EN ORO CON COLGANTE HEXAGONAL CON CIRCONITAS BLANCAS'
+            'titulo' => 'NOTA 2',
+            'texto' => 'ESTO ES UNA NOTA DE PRUEBA 2'
+        ]); Notas::create([
+            'titulo' => 'NOTA 3',
+            'texto' => 'ESTO ES UNA NOTA DE PRUEBA 3'
+        ]); Notas::create([
+            'titulo' => 'NOTA 4',
+            'texto' => 'ESTO ES UNA NOTA DE PRUEBA 4'
+        ]); Notas::create([
+            'titulo' => 'NOTA 5',
+            'texto' => 'ESTO ES UNA NOTA DE PRUEBA 5'
+        ]); Notas::create([
+            'titulo' => 'NOTA 6',
+            'texto' => 'ESTO ES UNA NOTA DE PRUEBA 6'
+        ]); Notas::create([
+            'titulo' => 'NOTA 7',
+            'texto' => 'ESTO ES UNA NOTA DE PRUEBA 7'
+        ]); Notas::create([
+            'titulo' => 'NOTA 8',
+            'texto' => 'ESTO ES UNA NOTA DE PRUEBA 8'
+        ]); Notas::create([
+            'titulo' => 'NOTA 9',
+            'texto' => 'ESTO ES UNA NOTA DE PRUEBA 9'
+        ]); Notas::create([
+            'titulo' => 'NOTA 10',
+            'texto' => 'ESTO ES UNA NOTA DE PRUEBA 10'
+        ]); Notas::create([
+            'titulo' => 'NOTA 11',
+            'texto' => 'ESTO ES UNA NOTA DE PRUEBA 11'
+        ]); Notas::create([
+            'titulo' => 'NOTA 12',
+            'texto' => 'ESTO ES UNA NOTA DE PRUEBA 12'
+        ]); Notas::create([
+            'titulo' => 'NOTA 13',
+            'texto' => 'ESTO ES UNA NOTA DE PRUEBA 13'
+        ]); Notas::create([
+            'titulo' => 'NOTA 14',
+            'texto' => 'ESTO ES UNA NOTA DE PRUEBA 14'
+        ]); Notas::create([
+            'titulo' => 'NOTA 15',
+            'texto' => 'ESTO ES UNA NOTA DE PRUEBA 15'
+        ]); Notas::create([
+            'titulo' => 'NOTA 16',
+            'texto' => 'ESTO ES UNA NOTA DE PRUEBA 16'
+        ]); Notas::create([
+            'titulo' => 'NOTA 17',
+            'texto' => 'ESTO ES UNA NOTA DE PRUEBA 17'
+        ]); Notas::create([
+            'titulo' => 'NOTA 18',
+            'texto' => 'ESTO ES UNA NOTA DE PRUEBA 18'
+        ]); Notas::create([
+            'titulo' => 'NOTA 19',
+            'texto' => 'ESTO ES UNA NOTA DE PRUEBA 19'
+        ]); Notas::create([
+            'titulo' => 'NOTA 20',
+            'texto' => 'ESTO ES UNA NOTA DE PRUEBA 20'
         ]);
     }
 }
